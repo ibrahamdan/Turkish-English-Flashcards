42b90e28cbe84d7e09d0021e0e0987dcc3d0a17b
ibra <ibraheemhamdan02@gmail.com>
2023-12-13 18:14:06 +0300

    updated server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const connection = require('./db');
 
 app.set('view engine', 'ejs');
 
-// Fetch a random word from the database
+
 app.get("/", (req, res) => {
   connection.query('SELECT * FROM trtoen ORDER BY RAND() LIMIT 1', (err, results) => {
     if (err) {
@@ -14,11 +14,9 @@ app.get("/", (req, res) => {
       return;
     }
 
-    // Render the "index" view and pass the random word to it
+
     res.render("index", { word: results[0] });
   });
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+app.listen(3000, () => {});
